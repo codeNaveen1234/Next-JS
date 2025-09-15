7219e196d96b2117a39aacd75e0a29a84495c6e1
naveen <chennamsettynaveen945@gmail.com>
2025-09-15 15:20:26 +0530

    Initial commit: Next.js PWA project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import withPWA from 'next-pwa';
+
+const nextConfig = {
+  distDir:'build',
+  swcMinify:true,
+  reactStrictMode:true,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+  nx: {
+    svgr: false,
+  },
+  basePath: '',
+};
+
+const pwaConfig =withPWA({
+  dest:'public',
+  register:true,
+  skipWaiting:true,
+  disable:process.env.NODE_ENV === 'development',
+})
+
+export default pwaConfig(nextConfig);
